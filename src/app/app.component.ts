ba5a51bbe98133ab3049a0851e45a7cc3d0c7227
Cree <cree@provinsal.com>
2019-04-01 14:40:04 -0700

    Add files

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+import { WikipediaSearchService } from "./wikipedia-search.service";
+import { Subject } from "rxjs/Subject";
+
+//application wide shared Rx operators
+import "rxjs/add/operator/map";
+
+@Component({
+  moduleId: module.id,
+  selector: "app-root",
+  templateUrl: "app.component.html"
+})
+export class AppComponent {
+  items: Array<string>;
+  term$ = new Subject<string>();
+  constructor(private service: WikipediaSearchService) {
+    this.term$.subscribe(term => this.search(term));
+  }
+
+  search(term: string) {
+    this.service.search(term).subscribe(results => (this.items = results));
+  }
+}
